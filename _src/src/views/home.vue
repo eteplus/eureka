340fe9cbf8c124b82cd74faf2aedbf873d9a78b9
eteplus <eteplus@163.com>
2016-12-22 16:27:11 +0800

    fix: change post style

--- _src/src/views/home.vue
+++ _src/src/views/home.vue
@@ -2,8 +2,8 @@
 <div class="posts">
   <div class="page-subhead">最新文章(lastest articles)</div>
   <div class="pure-g">
-    <transition-group tag="div" name="list" class="post pure-u-1 pure-u-xl-1-2">
-      <post v-for="(post, index) in posts" :author="post.author" :title="post.title" :description="post.description" :date="post.date" :tags="post.tags" :url="post.url" :key="index"/>
+    <transition-group tag="div" name="list">
+      <post v-for="(post, index) in posts" :author="post.author" :title="post.title" :description="post.description" :date="post.date" :tags="post.tags" :url="post.url" :key="index" class="post pure-u-1 pure-u-xl-1-2"/>
     </transition-group>
   </div>
 </div>
